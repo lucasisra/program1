b54de4ed34723a8ef7466ecb111fc28d82be1c94
Israel Lucas Moreira <47799887+lucasisra@users.noreply.github.com>
2019-02-26 22:06:19 -0300

    Add files via upload

---
+++ CALCULADORA.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int X=0;
+	int Y=0;
+	int z=0;
+	
+	int A=0;
+	int B=0;
+	int C=0;
+	int D=0;
+	
+	printf("Calculadora \n");
+	printf("digite valor um valor \n");
+	scanf("\%i",&X);
+	printf("digite o segundo valor \n");
+	scanf("\%i",&Y);
+	printf("escolha a operacao matematica \n");
+	printf("A = + \n");
+	printf("B = - \n");
+	printf("C = * \n");
+	printf("D = / \n");
+}
